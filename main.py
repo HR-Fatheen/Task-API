898ad7856222123ef60fde851491bef8afdd15dd
Mohamed Fatheen HR <hrfatheen18@gmail.com>
2026-08-19 07:53:17 +0530

    Complete Stage 2: task retrieval endpoints

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , HTTPException
 
 app = FastAPI()
 
@@ -15,3 +15,33 @@ def health():
     return {
         "status" : "ok"
     }
+
+tasks = [
+    {
+        "id": 1,
+        "title": "Learn Python",
+        "done": False
+    },
+    {
+        "id": 3,
+        "title": "Test The Product",
+        "done": False
+    },
+    {
+        "id": 2,
+        "title": "Configure The DB",
+        "done": False
+    }
+]
+
+@app.get("/tasks")
+def task_manager():
+    return tasks
+
+@app.get("/tasks/{id}")
+def get_task(id: int):
+    for task in tasks:
+        if task["id"] == id:
+            return task
+
+    raise HTTPException(status_code=404, detail="Task not found")
